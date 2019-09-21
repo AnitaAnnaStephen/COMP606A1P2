7a2b8ae8a0154f654c427b9a3e153f9a4248cb71
Gautam <gautam3491@gmail.com>
2019-09-21 18:48:27 +1200

    get bookings after date filter

---
+++ Assignment1Part3/getbookings.php
@@ -0,0 +1,34 @@
+<?php
+include "dbconnect.php";
+//  echo $_POST['sid'];
+//  echo $_POST['sid1'];
+//  echo $_POST['empid'];
+ $output='';
+$sql = "select u.FirstName,u.LastName,b.* from bookings b,userdetails u where b.Email=u.Email
+and b.EmployeeId=".$_POST['empid']." AND b.BookingStatus<>'Cancelled' and BookedDate between '".$_POST['sid']."' and '".$_POST['sid1']."'";
+$result = mysqli_query($mysqli,$sql);
+
+while( $row = $result->fetch_assoc()){
+    
+    $output .= '   
+                  
+                <tr>  
+                <td >' . $row["MassageType"] . '</td>  
+                     <td >' . date('d-M-Y',strtotime($row['BookedDate'])) . '</td>  
+                  
+                     <td >' . $row["Bookedtime"] . '</td>  
+                       
+                     <td >' . $row["Duration"] . ' minutes</td>  
+                       
+                     <td >$' . $row["Amount"] . '</td>  
+                      
+                     <td >' . $row["FirstName"] . ' ' . $row["LastName"] . '</td>  
+                       
+                     <td >' . $row["BookingDate"] . '</td>  
+                      
+                     <td >' . $row["BookingTime"] . '</td>  
+                </tr>
+               ';
+        echo $output;
+}
+?>
