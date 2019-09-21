f2dee5098679f491b3b2a40f0cbf3b95b7336eb5
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-09-22 01:12:00 +1200

    Updated validations before edit

--- Assignment1Part3/EditDB.php
+++ Assignment1Part3/EditDB.php
@@ -1,20 +1,20 @@
 <!-- This page saves the data entered by user in to the database after required validations -->
 <?php
-$message='';
+
+session_start();
+
 if (isset($_POST['edit']))
 {
     require 'dbconnect.php';//Establishing connection with database
-    $bookeddate=$massagetype=$bookedtime=$therapist='';
+    $bookeddate=$massagetype=$bookedtime=$therapist=$message='';
     //Retreiving values sent by http post method
     $bookeddate=$_POST['BookedDate'];
     $bookedtime=$_POST['time'];
-    // $massagetype=$_POST['MassageType'];
-    //$bookingtime=$_POST['BookingTime'];
     $therapist=$_POST['employee'];
-    $bookingid=$_GET["id"];
+    $bookingid=$_SESSION['Id'];
     $today=date('Y/m/d');//getting current date
     $serviceid = $_POST['MassageType'];
-
+    $message=$_POST['message'];
     $sql1 = "select * from services where serviceid=".$serviceid;
     $result = mysqli_query($mysqli, $sql1);
     $row = mysqli_fetch_array($result);
@@ -22,20 +22,58 @@ if (isset($_POST['edit']))
     $m = $row['MassageType'];
     $d = $row['Duration'];
     $a = $row['Amount'];
+    $e =  $_SESSION['username'];
     
-        //Checking if the email already exists in the database
-      //  $sql="UPDATE Bookings SET MassageType='$massagetype',BookingDate='$bookingdate',BookingTime=$bookingtime,EmployeeId=$therapist
-       // WHERE BookingId='$bookingid'";
-        $sql="UPDATE Bookings SET MassageType='$m',BookedDate='$bookeddate', Bookedtime='$bookedtime',
-        Duration=$d, Amount=$a, ServiceId=$s, EmployeeId=$therapist
+     $todate=date('Y-m-d');//getting current date
+    //Checking if time slot is already booked for the therapist by another user
+    $sql = "select * from bookings where BookingStatus<>'Cancelled' and employeeid=" . $therapist . " and bookedtime='" . $bookedtime . "' and bookeddate='" . $bookeddate . "'";
+    $result = mysqli_query($mysqli, $sql);
+    $row = mysqli_num_rows($result);
+    
+    if ($row == 0) 
+    {//Checking if time slot is already booked by the same user
+        $sql1 = "select * from bookings where BookingStatus<>'Cancelled' and email='" . $e . "' and serviceid=" . $s . " and bookedtime='" . $bookedtime . "' and bookeddate='" . $bookeddate . "'";
+        $result1 = mysqli_query($mysqli, $sql1);
+        $row1 = mysqli_num_rows($result1);
+        if ($row1 == 0) {
+            if($bookeddate=='0000-00-00' ||$bookeddate =='')//Checking if date is selected
+                {
+                    $_SESSION['message'] = 'Please select a date!!';
+                    header("Location: BookAppointment.php");
+                }
+             else if($bookeddate>=$todate )//Checking if booking is a past date
+                {
+                    $sqlup="UPDATE Bookings SET MassageType='$m',BookedDate='$bookeddate', Bookedtime='$bookedtime',
+                     Duration=$d, Amount=$a, ServiceId=$s, EmployeeId=$therapist,MessageforTherapist='$message'
+                     WHERE BookingId='$bookingid'"; 
+                    if (!$mysqli->query($sqlup)) { //If query execution failed.
+                        $_SESSION['messages']= 'Failed to update';
+                     echo ($mysqli->error);
+                    }
+                     else {
+                        $_SESSION['messages'] = 'Record updated successfully!!';
+                        header("Location: UserHome.php"); //redirecting to Home page                       
+                        }
+                }
+           
+                else
+                {
+                    $_SESSION['messages'] = 'Cannot book for past dates!!';
+                    header("Location: UserHome.php");
+                }
+                   
+                
+            
+        } 
+        else {
+            $_SESSION['messages'] = 'You Already Booked for this time slot!!';
+            header("Location: UserHome.php");
+             }
+    } else {
 
-        WHERE BookingId='$bookingid'";
-        //$result=mysqli_query($mysqli,$sql);
-        if ($mysqli->query($sql) === TRUE) {
-            echo "Record updated successfully";
-        } else {
-            echo "Error updating record: " . $mysqli->error;
-        }
+        $_SESSION['messages'] = 'Therapist unavailable!!';
+        header("Location: UserHome.php");
+    }
                 
           }     
    
