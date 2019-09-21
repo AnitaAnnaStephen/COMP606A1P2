3f6ba56fdf755b011ec990209b2c92b47a35d2c9
Gautam <gautam3491@gmail.com>
2019-09-21 19:27:26 +1200

    to update booking codes

--- Assignment1Part3/EditDB.php
+++ Assignment1Part3/EditDB.php
@@ -4,19 +4,31 @@ $message='';
 if (isset($_POST['edit']))
 {
     require 'dbconnect.php';//Establishing connection with database
-    $bookingdate=$massagetype=$bookingtime=$therapist='';
+    $bookeddate=$massagetype=$bookedtime=$therapist='';
     //Retreiving values sent by http post method
-    $bookingdate=$_POST['BookingDate'];
-    $massagetype=$_POST['MassageType'];
+    $bookeddate=$_POST['BookedDate'];
+    $bookedtime=$_POST['time'];
+    // $massagetype=$_POST['MassageType'];
     //$bookingtime=$_POST['BookingTime'];
     $therapist=$_POST['employee'];
     $bookingid=$_GET["id"];
     $today=date('Y/m/d');//getting current date
+    $serviceid = $_POST['MassageType'];
+
+    $sql1 = "select * from services where serviceid=".$serviceid;
+    $result = mysqli_query($mysqli, $sql1);
+    $row = mysqli_fetch_array($result);
+    $s = $row['ServiceId'];
+    $m = $row['MassageType'];
+    $d = $row['Duration'];
+    $a = $row['Amount'];
     
         //Checking if the email already exists in the database
       //  $sql="UPDATE Bookings SET MassageType='$massagetype',BookingDate='$bookingdate',BookingTime=$bookingtime,EmployeeId=$therapist
        // WHERE BookingId='$bookingid'";
-        $sql="UPDATE Bookings SET MassageType='$massagetype',BookingDate='$bookingdate',EmployeeId=$therapist
+        $sql="UPDATE Bookings SET MassageType='$m',BookedDate='$bookeddate', Bookedtime='$bookedtime',
+        Duration=$d, Amount=$a, ServiceId=$s, EmployeeId=$therapist
+
         WHERE BookingId='$bookingid'";
         //$result=mysqli_query($mysqli,$sql);
         if ($mysqli->query($sql) === TRUE) {
