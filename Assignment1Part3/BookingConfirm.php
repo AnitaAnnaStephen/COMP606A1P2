1c83008dbf0b694152403ce1c9c8e7cc1968be34
Gautam <gautam3491@gmail.com>
2019-09-22 11:19:47 +1200

    Booking Confirm pge

---
+++ Assignment1Part3/BookingConfirm.php
@@ -0,0 +1,37 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <title>View Booking</title>
+    <style>
+        .modal-xlg {
+            width: 100%;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="add_data_Modal" class="container">
+        <div class="modal-dialog modal-xlg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <!-- <button type="button" class="close" data-dismiss="modal">&times;</button>   -->
+                    <h4 class="modal-title"><strong>Booking Confirmed</strong></h4>
+                </div>
+                <div class="modal-body">
+
+                    <div>
+                    <p class="alert alert-success">Booking Successfull. You can view your bookings in your homepage</p>
+                    <p class="alert alert-success">Thank You for choosing us...</p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a href="UserHome.php" class="btn btn-info">View my Bookings</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
