4113f89d06885c7b88e16ae287212dd203326f13
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-09-22 01:14:16 +1200

    updated flexbox properties

--- Assignment1Part3/EmployeeHome.php
+++ Assignment1Part3/EmployeeHome.php
@@ -18,7 +18,7 @@
 
 .flex-container > div {
   background-color: #f1f1f1;
-  width: 260px;
+  width: 300px;
   margin: 10px;
   text-align: center;
   line-height: 75px;
@@ -43,7 +43,7 @@ if (isset($_SESSION['firstname']))
 }
 ?>
 <div class="flex-container">
-  <div><a href="ViewBookings.php">View Bookings</a></div>
+  <div><a href="ViewBookings.php">View Your Bookings</a></div>
    
 </div>
 <a href="Logout.php" style="font-weight:bold;background-color:Blue;">Logout</a>
