0e4ecbce6af338398f6cd3fb111598e8f6348357
Anita Anna Stephen <anitaanna.stephen@gmail.com>
2019-09-22 19:43:41 +1200

    Added comments

--- Assignment1Part3/BookingConfirm.php
+++ Assignment1Part3/BookingConfirm.php
@@ -1,5 +1,5 @@
+<!-- Page displaying booking confirmation. -->
 <html>
-
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <title>View Booking</title>
