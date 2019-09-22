0952f038d38924e26257a4773d76affe8ea0596e
Gautam <gautam3491@gmail.com>
2019-09-22 15:32:37 +1200

    changes to check which field are changed

--- Assignment1Part3/Edit.php
+++ Assignment1Part3/Edit.php
@@ -47,18 +47,21 @@ $_SESSION['Id']=$bookingid;
                                         ?>
                                         <option value='<?php echo $serviceid ?>' <?php if ($serviceid == $s) echo 'selected'; ?>><?php echo $service ?></option>
                                    <?php } ?>
+                                   <input type="hidden" name="firstMassageType" value="<?php echo $s; ?>" />
                               </select>       
                               <br />
                               <label>Select Date </label>
                               <input type="date" value="<?php echo $d; ?>"  name="BookedDate" id="bookingdate" required="true" min="1900-01-01" title="No past dates." style="border-radius:10px;height: 25px;">
+                              <input type="hidden" name="firstDate" value="<?php echo $d; ?>" />
                               <br />
                               <br />
                               <label>Select Time</label>
                               <select name="time" id="time" class="form-control"> 
-                                   <option value="8:00" <?php if ($t == '08:00:00') echo 'selected'; ?>>8:00 AM</option>
-                                   <option value="8:35" <?php if ($t == '08:35:00') echo 'selected'; ?>>8:35 AM</option>
-                                   <option value="9:10" <?php if ($t == '09:10:00') echo 'selected'; ?>>9:10 AM</option>
-                                   <option value="9:45" <?php if ($t == '09:45:00') echo 'selected'; ?>>9:45 AM</option>
+                                   <option value="08:00:00" <?php if ($t == '08:00:00') echo 'selected'; ?>>8:00 AM</option>
+                                   <option value="08:35:00" <?php if ($t == '08:35:00') echo 'selected'; ?>>8:35 AM</option>
+                                   <option value="09:10:00" <?php if ($t == '09:10:00') echo 'selected'; ?>>9:10 AM</option>
+                                   <option value="09:45:00" <?php if ($t == '09:45:00') echo 'selected'; ?>>9:45 AM</option>
+                                   <input type="hidden" name="firstTime" value="<?php echo $t; ?>" />
                               </select>
                               <br />
 
@@ -73,10 +76,12 @@ $_SESSION['Id']=$bookingid;
                                         $employees = $row2['FirstName'];
                                         $employeeid = $row2['EmployeeId'];
                                         ?>
-                                        <option value='<?php echo $employeeid; ?>' <?php if ($e == $employeeid) echo 'selected'; ?>><?php echo $employees; ?></option>";
+                                        <option value='<?php echo $employeeid; ?>' <?php if ($e == $employeeid) echo 'selected'; ?>><?php echo $employees; ?></option>
+                                        
                                    <?php } ?>
+                                   <input type="hidden" name="firstTherapist" value="<?php echo $e; ?>" />
                               </select>
-
+                              
                               <br />
                               <label for="comment">Motivation for making the Appointment:</label>
                           <textarea class="form-control" rows="5" id="message" name="message"value="<?php echo $m;?>"><?php echo $m;?></textarea>
